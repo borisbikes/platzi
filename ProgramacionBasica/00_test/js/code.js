40de6f9ed4fe467d1f1ff720512bb180050f6a47
jaircast <jair@cast.com>
2021-04-02 22:03:45 -0500

    First challenge Html and Css Curse

---
+++ ProgramacionBasica/00_test/js/code.js
@@ -0,0 +1,13 @@
+//alert("Hi");
+
+var c = 2;
+var na = "na";
+while(c){
+    console.log(c);
+    na += na;
+    c -= 1;
+}
+console.log(na + "Freddy");
+
+console.log(Math.ceil(4.64));
+console.log(Math.floor(4.64));
